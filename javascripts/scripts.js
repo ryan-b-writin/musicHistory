05584f30cc7076ac55b502877b49eba4adfa0158
Ryan B <ryan.bliton@gmail.com>
2016-05-16 21:22:40 -0500

    now with working edit buttons

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,100 +2,120 @@ $(document).ready(function(){
   var initialOffset = 0;
   var songList = []
 
-  //hide all display elements and then make selected ones visible
-  $("#addLink").click(function() {
+  function showAddView(){
     $("#defView").css("display", "none");
     $("#addView").css("display", "none");
     $("#viewLink").removeClass("selected");
     $("#addLink").addClass("selected");
     $("#addView").css("display", "block");
-  });
+  }
 
-  //hide all display elements and then make selected ones visible
-  $("#viewLink").click(function() {
+  function showListView(){
     $("#defView").css("display", "none");
     $("#addView").css("display", "none");
     $("#addLink").removeClass("selected");
     $("#viewLink").addClass("selected");
     $("#defView").css("display", "block");
+  }
+
+  //hide all display elements and then make selected ones visible
+  $("#addLink").click(function() {
+    showAddView();
+  });
+
+  //hide all display elements and then make selected ones visible
+  $("#viewLink").click(function() {
+    showListView();
   });
 
   //XHR request. accepts callback function, target div FOR callback function,
   // the requested JSON, and an offset number so the delete buttons work
-  function requestSongs(callback, targetDiv, targetJSON, offset) {
+  function requestSongs() {
     $.ajax({
-      url: targetJSON
+      url: "https://muzakhistory.firebaseio.com/songs.json"
     }).done(function(data){
-      for ( song in data ) {
-        songList.unshift(data[song]);
-      }
-      callback(targetDiv, offset);
+      populateList(data)
     });
   }
 
-  //populate initial nowPlaying list with data from song array.
-  //offset number allows delete buttons to have unique IDs when another JSON file is added.
-  var populateList = function(targetDiv,offset) {
+  function populateList(data) {
     var currentButton;
-    // console.log(songList);
-    // targetDiv.html(""); //nowplayingList
-    for (var i=0;i<songList.length;i++) {
-      targetDiv.append("<div><h2>Song Name: "+songList[i].name+ "</h2> <ul> <li>Artist Name: "+songList[i].artist+ "</li> <li>Album Name: "+songList[i].album+"</li> <li>Genre: "+songList[i].genre+"</li> <button class='deleteButton' id='del"+(i+offset)+"'>DELETE</button></ul></div>");
-      currentButton = $("#del"+(i+offset));
-      currentButton.click(function(){
-        var divToRemove = this.parentNode.parentNode
-        this.parentNode.parentNode.parentNode.removeChild(divToRemove);
+    var editArray= "";
+    for ( song in data ) {
+      $("#nowPlayingList").append(`<div><h2>Song Name: ${data[song].name}</h2>`)
+      $("#nowPlayingList").append(`<ul><li>Artist Name: ${data[song].artist}</li><li>Album Name: ${data[song].album}</li><li>Genre: ${data[song].genre}</li></ul></div>`)
+      $("#nowPlayingList").append(`<button id="${song}" class="deleteButton">DELETE</button>`)
+      $("#nowPlayingList").append(`<button id="edit_${song}">EDIT</button`)
+      deleteButton = $(`#${song}`);
+      deleteButton.click(function(){
+        deleteSong(this.id)
+      })
+      editButton = $(`#edit_${song}`);
+      editButton.click(function(){
+        editArray = this.id;
+        var a = editArray.split("edit_");
+        showAddView();
+        $.ajax({
+          url: `https://muzakhistory.firebaseio.com/songs/${a[1]}.json`,
+          method: "GET"
+        }).done(function(data){
+          $("#SName").val(data.name),
+          $("#SArtist").val(data.artist),
+          $("#SAlbum").val(data.album),
+          $("#SGenre").val(data.genre),
+          $(".addButton").attr('id', a[1])
+        })
       })
     }
-    songList=[];
-  };
+  }
 
-  //load first JSON
-  requestSongs(populateList, $("#nowPlayingList"), "https://muzakhistory.firebaseio.com/songs.json", initialOffset);
+function deleteSong(songId) {
+  $.ajax({
+    url: `https://muzakhistory.firebaseio.com/songs/${songId}.json`,
+    method: "DELETE"
+  }).done(function(){
+    $("#nowPlayingList").html("");
+    requestSongs();
+  })
+}
 
-  // add event listener to "add button"
-  // $("#addButton").click(function(){
-  //   //on click, capture input values & add them to array of objects
-  //   var newSong = {};
-  //   newSong.name = $("#SName").val();
-  //   newSong.artist = $("#SArtist").val();
-  //   newSong.album = $("#SAlbum").val();
-  //   newSong.genre = $("#SGenre").val();
-  //   console.log("newsong", newSong );
-  //   songList.unshift(newSong);
-  //   console.log("songlist", songList );
-  //   initialOffset += 200;
-  //   initialOffset++;
-  //   populateList($("#nowPlayingList"),initialOffset);
-  //   $("#SName").val("")
-  //   $("#SArtist").val("")
-  //   $("#SAlbum").val("") 
-  //   $("#SGenre").val("")
-  // })
-
-  $("#addButton").click(function(){
-    var newSong = {
-      "name": $("#SName").val(),
-      "artist": $("#SArtist").val(),
-      "album": $("#SAlbum").val(),
-      "genre": $("#SGenre").val()
-    };
-    $.ajax({
-      url: 'https://muzakhistory.firebaseio.com/songs.json',
-      type: 'POST',
-      data: JSON.stringify(newSong)
-    }).done(function(){
+function clearAndReset(){
       $("#nowPlayingList").html("");
-      requestSongs(populateList, $("#nowPlayingList"), "https://muzakhistory.firebaseio.com/songs.json", initialOffset);
-    })
-    $("#SName").val("");
-    $("#SArtist").val("");
-    $("#SAlbum").val("");
-    $("#SGenre").val("");
+      requestSongs();
+      showListView();
+      $("#SName").val("");
+      $("#SArtist").val("");
+      $("#SAlbum").val("");
+      $("#SGenre").val("");
+}
+
+  $(".addButton").click(function(){
+      var newSong = {
+        "name": $("#SName").val(),
+        "artist": $("#SArtist").val(),
+        "album": $("#SAlbum").val(),
+        "genre": $("#SGenre").val()
+      };
+    if ($(this).attr("id")){
+      var editTag = ($(this).attr("id"))
+      $.ajax({
+        url: `https://muzakhistory.firebaseio.com/songs/${editTag}.json`, 
+        type: "PUT",
+        data: JSON.stringify(newSong)
+      }).done(function(){
+        clearAndReset()
+      })
+    } else {
+      $.ajax({
+        url: 'https://muzakhistory.firebaseio.com/songs.json',
+        type: 'POST',
+        data: JSON.stringify(newSong)
+      }).done(function(){
+        clearAndReset()
+      })
+    } 
   })
 
-  //add event listener to "more button"
-//   $("#more").click(function(){
-//     requestSongs(populateList, $("#nowPlayingList"),"../songs2.json",5)
-//   });
+requestSongs();
+
 })
